5502363fd975d66e71a9673be5ff9758d5191531
haoyang <phymailforwork@gmail.com>
2025-08-11 04:06:37 +0800

    chore: types

---
+++ src/types/component.ts
@@ -0,0 +1,28 @@
+export interface ComponentOptions extends LicyCycleHooks {
+  $type?: 'vue2' | 'vue3'
+  name?: string
+  template?: string
+  components?: Record<string, ComponentOptions>
+  data?: (() => Record<string, any>) | Record<string, any>
+  methods?: Record<string, (this: any) => void>
+  setup?: () => Record<string, any>
+  computed?: Record<string, Function>
+  watch?: Record<string, Function>
+  render?: (h: Function) => string
+  props?: Record<string, any>
+  directives?: Record<string, Function>
+  filters?: Record<string, Function>
+  provide?: () => Record<string, any>
+  inject?: string[]
+}
+
+interface LicyCycleHooks {
+  beforeCreate?: () => void
+  created?: () => void
+  beforeMount?: () => void
+  mounted?: () => void
+  beforeUpdate?: () => void
+  updated?: () => void
+  beforeDestroy?: () => void
+  destroyed?: () => void
+}

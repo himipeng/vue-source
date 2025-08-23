72a9531bdf79e4c5187144877f002b4aa11066ef
haoyang <phymailforwork@gmail.com>
2025-08-24 00:05:41 +0800

    feat: 实现插件系统

--- src/code/runtime-dom/createApp.ts
+++ src/code/runtime-dom/createApp.ts
@@ -3,6 +3,7 @@ import * as nodeOps from './nodeOps'
 import type { ComponentOptions } from '@/types/runtime-core'
 import { createVNode } from '@vue/runtime-core'
 import { patchProp } from './patchProp'
+import { isFunction } from '@/utils'
 
 /**
  * Vue App 实例接口
@@ -46,14 +47,31 @@ export interface App<HostElement = Element> {
   _container: HostElement | null
 }
 
+type PluginInstallFunction<Options = any[]> = Options extends unknown[]
+  ? (app: App, ...options: Options) => any
+  : (app: App, options: Options) => any
+
+export type ObjectPlugin<Options = any[]> = {
+  install: PluginInstallFunction<Options>
+}
+export type FunctionPlugin<Options = any[]> = PluginInstallFunction<Options> & Partial<ObjectPlugin<Options>>
+
+export type Plugin<Options = any[], P extends unknown[] = Options extends unknown[] ? Options : [Options]> =
+  | FunctionPlugin<P>
+  | ObjectPlugin<P>
+
 const renderer = createRenderer({ ...nodeOps, patchProp })
 
 /**
  * 创建应用实例
  * @param rootComponent 根组件
  * @returns App 实例，包含 mount、use 等方法
+ * Vue 团队刻意避免用 Class（轻量、便于tree-shaking）
  */
 export function createApp(rootComponent: ComponentOptions): App {
+  /** 已注册的插件 */
+  const installedPlugins = new WeakSet()
+
   const app: App = {
     _container: null,
     mount(rootContainer: Element | string) {
@@ -72,11 +90,31 @@ export function createApp(rootComponent: ComponentOptions): App {
       return vnode.component!.proxy
     },
 
-    // TODO
-    unmount() {},
-    use() {
+    use(plugin: Plugin, ...options: any[]) {
+      // 1. 防止重复安装
+      if (installedPlugins.has(plugin)) {
+        console.warn(`Plugin has already been applied to target app.`)
+      }
+      // 2. 对象插件：plugin.install(app, ...options)
+      else if (plugin && isFunction(plugin.install)) {
+        installedPlugins.add(plugin)
+        plugin.install(app, ...options)
+      }
+      // 3. 函数插件：plugin(app, ...options)
+      else if (isFunction(plugin)) {
+        installedPlugins.add(plugin)
+        plugin(app, ...options)
+      }
+      // 4. 不合法的插件
+      else {
+        console.warn(`A plugin must either be a function or an object with an "install" ` + `function.`)
+      }
+
       return app
     },
+
+    // TODO
+    unmount() {},
     component(name: string, component: any) {
       return app
     },

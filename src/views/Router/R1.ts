1b6dd0a11f7def20c816f77623d66132ab005bdc
haoyang <phymailforwork@gmail.com>
2025-09-02 01:36:47 +0800

    feat: view router

--- src/views/Router/R1.ts
+++ src/views/Router/R1.ts
@@ -3,6 +3,11 @@ import { defineComponent } from 'vue'
 const template = `
   <div>
     <p>R1</p>
+    <div>
+      <router-link to="/router/r1/r3"></router-link>
+      |
+      <router-link to="/router/r1/r4"></router-link>
+    </div>
     <router-view></router-view>
   </div>
 `
